a07febcaad171bcac77d205ec6fb0f046015a85f
RichardFuentes369 <javierbaron6@gmail.com>
2025-03-25 18:35:58 -0500

    first commit

---
+++ src/mod/index.ts
@@ -0,0 +1,11 @@
+export { AuthadminModule } from './user/admin/auth/auth.module'
+export { AdminModule } from './user/admin/user/admin.module';
+
+export { AuthuserModule } from './user/final/auth/auth.module'
+export { UserModule } from './user/final/user/user.module';
+
+export { AsignacionModule } from './user/admin/permission/asignacion/asignacion.module'
+export { ModulosModule } from './modules/modulos.module'
+
+
+
